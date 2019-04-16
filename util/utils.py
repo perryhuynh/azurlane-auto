5a2f629ed4b8c6582381fd27a175689dea784ad4
wycissiily <jerry050428@outlook.com>
2019-04-16 17:42:43 +0800

    Chinese version

--- util/utils.py
+++ util/utils.py
@@ -387,6 +387,4 @@ class Utils(object):
             in the list of coordinates to the specified coordinate as well the
             index of where it is in the list of coordinates
         """
-        print(coords)
-        print(coord)
         return spatial.KDTree(coords).query(coord)
